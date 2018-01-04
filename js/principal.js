f524dc7471e90153ea28d4bcec6940c2bcd511bd
Everton Walker <everton.walker@hotmail.com>
2018-01-04 14:06:26 -0200

    Refatoração do código

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,16 @@
+ 		//Capturando o form;
+ 		var form = document.querySelector("#form-adiciona");
+
+ 		//Capturando a Tabela;
+		 var tabela = document.querySelector("#tabela-pacientes");
+
+		//Capturando o botão do dom que será Clicado.
+		 var button = document.querySelector("#adicionar-paciente");
+
+		//Capturando os pacientes para calcular seus imcs 
+		var pacientes = document.querySelectorAll(".paciente");
+
+ 		//Função responsável por verificar os dados do imc e adicionar marcação a TR responsável pelos erros.
  		function verificarImc(imc, tag){
 
 		 if(imc == "Dados do paciente são inválidos."){
@@ -17,25 +30,72 @@
 			return (peso/ (altura * altura)).toFixed(2);
 		}
 
+		//função que recebe os dados do formulário e converte em um paciente no formato JSON
+		function obterPacienteFormulario(){
+			
+			var paciente = {
+				
+				nome : this.form.nome.value,
+				altura: this.form.altura.value,
+				peso: this.form.peso.value,
+				gordura: this.form.gordura.value,
+				imc: imc(this.form.peso.value, this.form.altura.value)
+			}
 
-		//O DOM, ou document object Model carrega todo conteúdo da página, caso queira ver é só usar um
-		console.log(document);
+			return paciente;
+		}
 
-		//Através desse objeto temos uma serie de métodos que podem ser utilizados para manipulação
-		//da página, por exemplo aqui capturamos todos conteúdos da primeira tag H1, dentro do DOM
+		//Retorna uma TD, com o dado já inserido nela e uma classe caso você queira adicionar
+		function montarTd(dado, classe){
+			
+			var td = document.createElement("td");
+			td.textContent = dado;
+			td.classList.add(classe);
 
-		//é valido lembrar que ele seleciona apenas o primeiro, por isso é valido usar names,classes ou ids para as tags.
-		//var teste = document.querySelector("h1");
-		var titulo = document.querySelector(".titulo");
+			return td;
 
-		//Guardamos isso em uma variável e com o atributo textContent, iremos manipular o seu conteúdo
-		titulo.textContent = "Everton Walker";
+		}
 
-		//Capturando todos dados de quem possui classs com nome Paciente e guardando em uma variável
-		
-		var pacientes = document.querySelectorAll(".paciente");
+		//função responsável por montar toda uma TR, colocar as TD's, passar os valores e retornar ela;
+		function montarTr(paciente){
 
-		pacientes = document.querySelectorAll(".paciente");
+			var pacienteTr = document.createElement("tr");
+			pacienteTr.classList.add(".paciente");
+
+		 	verificarImc(paciente.imc, pacienteTr);
+
+		 	//Adicionar Td's ao paciente TD. 
+		 	pacienteTr.appendChild(montarTd(paciente.nome, "info-nome"));
+		    pacienteTr.appendChild(montarTd(paciente.peso, "info-peso"));
+		    pacienteTr.appendChild(montarTd(paciente.altura, "info-altura"));
+		    pacienteTr.appendChild(montarTd(paciente.gordura, "info-gordura"));
+		    pacienteTr.appendChild(montarTd(paciente.imc, "info-imc"));
+
+		    return pacienteTr;
+
+		}
+
+
+
+		function adicionarPaciente(){
+
+			//retirando o comportamento padrão de recarregar a página ao subimeter um formulário.
+		 	event.preventDefault();
+
+		 	//Retornando o paciente montando no formato JSON;
+		 	var paciente = obterPacienteFormulario();
+		 	//Retornando uma TR toda preenchida;
+		 	var pacienteTr = montarTr(paciente);
+		 	//Adicionando a TR a Tabela.
+		 	tabela.appendChild(pacienteTr);
+ 		
+ 			//Limpando os dados do formulário:
+ 			form.reset();
+
+		}
+	
+		 //Adicionando um evento ao botão.
+		 button.addEventListener("click", adicionarPaciente);
 	
 		//executo um loop para cada paciente encontrado, cálculo seu IMC E EXIBO na tela.
 		for(var i = 0; i < pacientes.length; i++){
@@ -47,6 +107,28 @@
 		verificarImc(pacientes[i].querySelector(".info-imc").textContent, pacientes[i]);
 
 		}
+
+
+
+
+
+
+		// ---------------->> O CÓDIGO DAQUI PRA BAIXO É NÍVEL de aprendizado <<----------
+
+		 //O DOM, ou document object Model carrega todo conteúdo da página, caso queira ver é só usar um
+		console.log(document);
+
+		//Através desse objeto temos uma serie de métodos que podem ser utilizados para manipulação
+		//da página, por exemplo aqui capturamos todos conteúdos da primeira tag H1, dentro do DOM
+
+		//é valido lembrar que ele seleciona apenas o primeiro, por isso é valido usar names,classes ou ids para as tags.
+		//var teste = document.querySelector("h1");
+		var titulo = document.querySelector(".titulo");
+
+		//Guardamos isso em uma variável e com o atributo textContent, iremos manipular o seu conteúdo
+		titulo.textContent = "Everton Walker";		
+
+			
 		//Se os dados forem inválidos com o método style.color ele altera todo estilo do paciente, a fim de facilitar a visualização
 		//pacientes[i].style.color = "red";
 		//Mas caso todo site deveriar ser alterado então deveríamos colocar isson o CSS e assim a alteração seria genêrica
@@ -68,59 +150,3 @@
 			var nome = document.querySelector("#nome-input");
 			nome.value;
 		 */
-
-
-		 //Selecionando o botão do dom que será Clicado.
-		 var button = document.querySelector("#adicionar-paciente");
-
-		 //Adicionando um evento ao botão.
-		 button.addEventListener("click", function(){
-		 	
-		 	//retirando o comportamento padrão de recarregar a página ao subimeter um formulário.
-		 	event.preventDefault();
-
-		 	//Capturando o form;
-		 	var form = document.querySelector("#form-adiciona");
-
-		 	//Capturando cada valor digitado nos inputs.
-			var nome = form.nome.value;
-		 	var gordura = form.gordura.value;
-		 	var altura = form.altura.value;
-		 	var peso = form.peso.value;
-		 	
-		 	//Criando as tags que serão utilizadas para adicionar o paciente: TR e TD'S
-		 	var pacienteTr = document.createElement("tr");
-		 	var nomeTd = document.createElement("td");
-		 	var gorduraTd = document.createElement("td");
-		 	var alturaTd = document.createElement("td");
-		 	var pesoTd = document.createElement("td");
-		 	var imcTd = document.createElement("td");
-
-		 	//Passando o valor obtido pelos inputs para cada TD.
-		 	nomeTd.textContent = nome;
-		 	gorduraTd.textContent = gordura;
-		 	alturaTd.textContent = altura;
-		 	pesoTd.textContent = peso;
-		 	imcTd.textContent = imc(peso, altura);
-
-		 	verificarImc(imcTd.textContent, pacienteTr);
-
-		 	//Adicionando todas TD ao seu pai TR.
-		 	pacienteTr.appendChild(nomeTd);
-		    pacienteTr.appendChild(pesoTd);
-		    pacienteTr.appendChild(alturaTd);
-		    pacienteTr.appendChild(gorduraTd);
-		    pacienteTr.appendChild(imcTd);
-		 	
-		 	//Capturando a Tabela e adicionando a TR com o paciente criado.
-		 	var tabela = document.querySelector("#tabela-pacientes");
-		 	tabela.appendChild(pacienteTr);
- 
-
-		 });
-
-		 
-
-
-
-		
